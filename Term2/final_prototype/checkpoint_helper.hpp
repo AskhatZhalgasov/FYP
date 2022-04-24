14faf07f5e034a1082cf96dce37d80fe2cf852b6
Askhat Zhalgasov <zhalgasovaskhat@gmail.com>
2022-04-24 23:01:30 +0800

    clean up

--- Term2/final_prototype/checkpoint_helper.hpp
+++ Term2/final_prototype/checkpoint_helper.hpp
@@ -2,6 +2,44 @@
 
 #include <bits/stdc++.h>
 
+struct CheckpointHelper;
+
+#define named_label(a, b) concat(a, b) 
+#define concat(a, b) a ## b
+#define unique_label named_label(label, __LINE__)
+
+#define checkpoint_await_with_label(label_name, coro) \
+  label_name:                                   \
+  checkpointer->resume_point = &&label_name;    \
+  co_await coro;                                
+
+#define checkpoint_await(coro) \
+  checkpoint_await_with_label(unique_label, coro)
+
+#define checkpoint_with_label(label_name)     \
+  label_name:                                 \
+  checkpointer->resume_point = &&label_name;  \
+  checkpointer->serialize();
+
+#define checkpoint_save() \
+  checkpoint_with_label(unique_label)
+
+#define checkpoint_init() \
+  auto checkpointer = co_await GetCheckpoint<Task::promise_type>{}; \
+  if (checkpointer->resume_point != nullptr) \
+    goto *checkpointer->resume_point; 
+
+
+template<typename PromiseType> struct GetCheckpoint {
+  CheckpointHelper* _checkpointer;
+  bool await_ready() { return false; } // says yes call await_suspend
+  bool await_suspend(std::coroutine_handle<PromiseType> handle) {
+    _checkpointer = &handle.promise().checkpointer;
+    return false;     // says no don't suspend coroutine after all
+  }
+  auto await_resume() { return _checkpointer; }
+};
+
 enum Status {
   Blocked = 0, 
   Running, 
